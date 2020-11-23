a220dd1a8ebb9efb02219960501bca9e35ea887f
Philipp Woerdehoff <philipp.woerdehoff@gmail.com>
2020-11-23 21:32:06 +0100

    .

--- src/index.js
+++ src/index.js
@@ -197,7 +197,7 @@ function shuffle(array) {
 const to = i => ({x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20})
 const from = i => ({x: 0, rot: 0, scale: 1.5, y: -1000})
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
+// const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
 const dirUp = -1
 const dirDown = 1
